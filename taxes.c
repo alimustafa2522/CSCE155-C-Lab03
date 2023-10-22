13810ebdec9d09ba7c05076db3873f7275bacf17
alimustafa <alimustafa2522@gmail.com>
2023-10-22 17:34:58 +0500

    copy iflse just change values

--- taxes.c
+++ taxes.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   double childCredit = 0.0;
   double totalTax = 0.0;
   int numChildren = 0;
+  double calculation;
 
   printf("Please enter your adjusted gross income (AGI): ");
   scanf("%lf", &agi);
@@ -33,11 +34,28 @@ int main(int argc, char **argv) {
                         }
      
    if(agi>0&&agi<19900)
+   // 10 per of AGI
      {
+      tax = (10*agi)/100;
+      totalTax = tax - childCredit;
+     }
 
-     }                  
+   else if(agi>19901 && agi<81050)
+   {
+     calculation = (agi - 19900);
 
-    
+    tax = ((12*calculation)/100)+1990;
+   }                
+
+  // copy the if else statements
+
+  else if(agi>=81051 && agi<=172750)
+  {
+    calculation = (agi - 81050);
+    tax = ((12*calculation)/100) + 9328;
+    totalTax = tax - childCredit;
+  }
+  // copy if else just change the values ;
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
